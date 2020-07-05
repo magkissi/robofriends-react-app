bc66b44533d646f73eb7c29595c6e76b6277e985
Magkissi <magkissi111@gmail.com>
2020-07-05 22:29:04 +0000

    adding react-redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,62 +1,68 @@
-import React, { Component } from 'react';
-import Cardlist from '../components/Cardlist';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import '../containers/App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Cardlist from "../components/Cardlist";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "../containers/App.css";
+import { setSearchField } from "../actions";
 
-import ErrorBoundry from '../components/ErrorBoundry';
-
+import ErrorBoundry from "../components/ErrorBoundry";
 
 //state allows the child component to communicate with the parent component //
 //super calls the constructor//
 
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
+
 class App extends Component {
-    constructor() {
-        super()  
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    
-}
-//the onSearchChange functions says; anytime the SearchBox component changes an should trigger an event, and the event will console log//
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+    };
+  }
+  //the onSearchChange functions says; anytime the SearchBox component changes an should trigger an event, and the event will console log//
 
-componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response=> response.json())
-    .then(users=> {this.setState({ robots: users})});
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
 
-}
-onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value })
-    
-    
-}
+  render() {
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filteredRobots = robots.filter((robots) => {
+      return robots.name.toLowerCase().includes(searchField.toLowerCase());
+    });
 
-    render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
-                
-            <div className='tc'>
-                    <h1 className="f1">RoboFriends</h1>
-                 <SearchBox searchChange={this.onSearchChange}/>
-                 <Scroll>
-                    <ErrorBoundry> 
-                    <Cardlist robots={filteredRobots} />
-                    </ErrorBoundry>
-                </Scroll>
-                
-            </div>
-                );
-        }
-    
-}
+    if (robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <Cardlist robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+    }
+  }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
-import App from './containers/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddlewear from "redux-thunk";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import App from "./containers/App";
+import { searchRobots } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root')
-          
+const logger = createLogger();
+const store = createStore(
+  searchRobots,
+  applyMiddleware(thunkMiddlewear, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
